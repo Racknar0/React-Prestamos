5ba5b18bc6f09129feb913b15a049b93f355ecb5
Racknar0 <racknarow1@gmail.com>
2022-03-06 14:48:33 -0500

    Hook State

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,57 +1,68 @@
-import React, {useState} from 'react';
+import React, { Fragment, useState } from 'react';
 
-const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
-
-
+const Formulario = ({ cantidad, guardarCantidad, plazo, guardarPlazo }) => {
     //! Defonor state
     const [error, guardarError] = useState(false);
 
-     //! Cuando el usuario hace el submit
-    const calcularPrestamo = e => {
-        e.preventDefault()
+    //! Cuando el usuario hace el submit
+    const calcularPrestamo = (e) => {
+        e.preventDefault();
 
         //!valida
-        if( cantidad === 0 || plazo === '' ) {
-            guardarError(true)
+        if (cantidad === 0 || plazo === '') {
+            guardarError(true);
+            return;
         }
 
+        //! eliminar el error previo
+        guardarError(false);
 
         //!realizar cotizacion
-    }
+    };
 
     return (
-        <form onSubmit={ calcularPrestamo }>
-            <div className="row">
-                <div>
-                    <label>Cantidad Prestamo</label>
-                    <input
-                        className="u-full-width"
-                        type="number"
-                        placeholder="Ejemplo: 3000"
-                        onChange={ e => guardarCantidad( parseInt ( e.target.value ) ) }
-                    />
+        <Fragment>
+            <form onSubmit={calcularPrestamo}>
+                <div className="row">
+                    <div>
+                        <label>Cantidad Prestamo</label>
+                        <input
+                            className="u-full-width"
+                            type="number"
+                            placeholder="Ejemplo: 3000"
+                            onChange={(e) =>
+                                guardarCantidad(parseInt(e.target.value))
+                            }
+                        />
+                    </div>
+                    <div>
+                        <label>Plazo para Pagar</label>
+                        <select
+                            className="u-full-width"
+                            onChange={(e) =>
+                                guardarPlazo(parseInt(e.target.value))
+                            }
+                        >
+                            <option value="">Seleccionar</option>
+                            <option value="3">3 meses</option>
+                            <option value="6">6 meses</option>
+                            <option value="12">12 meses</option>
+                            <option value="24">24 meses</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="Calcular"
+                            className="button-primary u-full-width"
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Plazo para Pagar</label>
-                    <select className="u-full-width"
-                    onChange={ e => guardarPlazo( parseInt ( e.target.value ) ) }
-                    >
-                        <option value="">Seleccionar</option>
-                        <option value="3">3 meses</option>
-                        <option value="6">6 meses</option>
-                        <option value="12">12 meses</option>
-                        <option value="24">24 meses</option>
-                    </select>
-                </div>
-                <div>
-                    <input
-                        type="submit"
-                        value="Calcular"
-                        className="button-primary u-full-width"
-                    />
-                </div>
-            </div>
-        </form>
+            </form>
+
+            {(error) ? <p className="error">Todos los campos son Obligatorios</p>  : null  }
+            
+        </Fragment>
     );
 };
 
